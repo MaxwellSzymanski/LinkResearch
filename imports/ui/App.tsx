fc2edadd06764c9f522ecc1bfbdc0213d2cc793b
Maxwell Szymanski <maxwell.szymanski@student.kuleuven.be>
2022-11-17 11:51:13 +0100

    Initial commit

---
+++ imports/ui/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Hello } from "./Hello";
+
+export const App = () => (
+  <div>
+    <h1 className="text-xl text-primary-700 font-display font-bold underline">
+      Hello world!
+    </h1>
+    <Hello />
+  </div>
+);
